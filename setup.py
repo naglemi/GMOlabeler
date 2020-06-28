9a45d805b36d8949363deec76b7d3b714a9c1b39
Michael Nagle <michael.nagle@oregonstate.edu>
2020-06-28 02:10:45 -0700

    Added setup file

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from gmo_labeler.core import setup
+
+setup(name='gmo_labeler',
+      version='0.1.53',
+      description='GMO labeler using hyperspectral image data and machine/deep learning',
+      author='Michael Nagle',
+      author_email='michael.nagle@oregonstate.edu',
+      packages=['pandas', 'numpy'],
+     )
